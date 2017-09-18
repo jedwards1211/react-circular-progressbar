c36a3cacbe12df90872985c5cb8ea6fd8257f80d
Andy Edwards <jedwards@fastmail.com>
2017-09-18 18:29:30 -0500

    export styles from 'index

--- src/index.jsx
+++ src/index.jsx
@@ -121,3 +121,31 @@ CircularProgressbar.defaultProps = {
 };
 
 export default CircularProgressbar;
+
+export const styles = {
+  root: {
+    /*
+     * This fixes an issue where the CircularProgressbar svg has
+     * 0 width inside a "display: flex" container, and thus not visible.
+     *
+     * If you're not using "display: flex", you can remove this style.
+     */
+    width: '100%',
+  },
+  path: {
+    stroke: '#3e98c7',
+    strokeLinecap: 'round',
+    transition: 'stroke-dashoffset 0.5s ease 0s',
+  },
+  trail: {
+    stroke: '#d6d6d6',
+  },
+  text: {
+    fill: '#3e98c7',
+    fontSize: 20,
+    dominantBaseline: 'middle',
+    textAnchor: 'middle',
+  },
+};
+
+
